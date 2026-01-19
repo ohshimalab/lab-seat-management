6a24da762b8ae0cd4d42a5548a81d20717cc486b
longpham28 <huulongpham28@gmail.com>
2026-01-19 23:11:39 +0900

    fix lint

--- src/__tests__/timeline.test.tsx
+++ src/__tests__/timeline.test.tsx
@@ -5,7 +5,6 @@ import {
   within,
   cleanup,
 } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import App from "../App";
 
